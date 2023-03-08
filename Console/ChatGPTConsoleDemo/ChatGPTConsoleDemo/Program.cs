219743cfce4a03d2278e5b9ccbdf171664d88365
dingxiaowei <1213250243@qq.com>
2023-03-08 15:17:28 +0800

    添加控制台Demo

---
+++ Console/ChatGPTConsoleDemo/ChatGPTConsoleDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ChatGPTConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetAnswer("海安"));
+            Console.Read();
+        }
+
+        public static string GetAnswer(string question)
+        {
+            return PostJson("https://www.pmzhang.cn/api/generate", "{\"messages\":[{\"role\":\"user\",\"content\":\"" + question + "\"}]}");
+        }
+
+        public static string PostJson(string url, string postJsonData)
+        {
+            string result = "";
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "POST";
+                req.ContentType = "text/plain;charset=UTF-8";
+                req.Accept = "*/*";
+                var data = Encoding.UTF8.GetBytes(postJsonData);
+                req.ContentLength = data.Length;
+                using (Stream reqStream = req.GetRequestStream())
+                {
+                    reqStream.Write(data, 0, data.Length);
+                    reqStream.Close();
+                }
+                var resp = (HttpWebResponse)req.GetResponse();
+                var stream = resp.GetResponseStream();
+
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    result = reader.ReadToEnd();
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Post异常:" + ex.ToString());
+            }
+            return "";
+        }
+    }
+}
